d8977a3c700cd9d99237b26c8732eda9034bcf53
nthndkid <nathanflrs813@gmail.com>
2025-07-08 11:16:05 +0800

    js-3 initial

--- Assignment-Exercises/JS-3_Conditional-Statements/script.js
+++ Assignment-Exercises/JS-3_Conditional-Statements/script.js
@@ -1,3 +1,16 @@
+const age = document.getElementById('ageInput').value;
+const result = document.getElementById('result');
+console.log(age);
 function checkAge() {
     // write your code here 
+
+    if(age <= 12){
+        result.innerHTML = `<p>You are a(n): Child</p>`;
+    } else if (age <= 19) {
+        result.innerHTML = `<p>You are a(n): Teenager</p>`;
+    } else if (age >= 20) {
+        result.innerHTML = `<p>You are a(n): Adult</p>`;
+    } else {
+        result.innerHTML = `<p>Invalid age. Please enter a valid nummber</p>`;
+    }
 }
