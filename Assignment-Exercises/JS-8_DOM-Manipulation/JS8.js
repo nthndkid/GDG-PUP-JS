693b596bebe9df2dae2cc56ae6504fadd8640941
nthndkid <143003322+nthndkid@users.noreply.github.com>
2025-07-09 17:35:53 +0800

    js-8  assignment done

--- Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
+++ Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
@@ -1,1 +1,17 @@
 // your code starts here
+const counter = document.getElementById("counter-text");
+const addBtn = document.getElementById("button-add");
+const subtractBtn = document.getElementById("button-subtract");
+let count = Number(counter.innerText);
+
+addBtn.addEventListener("click", function addCount(){
+    count++;
+    counter.innerText = count;
+});
+
+subtractBtn.addEventListener("click", function subtractCount(){
+    if(count > 0){
+        count--;
+        counter.innerText = count;
+    }
+});
