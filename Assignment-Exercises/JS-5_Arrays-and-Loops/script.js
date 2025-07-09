0d532817549be2bb806ca1b8a17169fd29e50df9
nthndkid <143003322+nthndkid@users.noreply.github.com>
2025-07-09 16:38:40 +0800

    js-5 assignment done

---
+++ Assignment-Exercises/JS-5_Arrays-and-Loops/script.js
@@ -0,0 +1,20 @@
+let favoriteMovies = [
+  "2012",
+  "The Matrix",
+  "Spider Man Far From Home",
+  "The Dark Knight",
+  "Forrest Gump",
+  "The Man Called Otto",
+  "Avengers: Endgame",
+  "Spirited Away",
+  "The Godfather",
+  "Your Name"
+];
+
+
+let index = 0;
+
+while(index < favoriteMovies.length){
+    console.log(favoriteMovies[index])
+    index++;
+}
