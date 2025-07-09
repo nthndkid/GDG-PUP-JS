68afa37fe71b78617fb1d899e4d5bf4ddd6ed357
nthndkid <143003322+nthndkid@users.noreply.github.com>
2025-07-09 16:43:41 +0800

    js-6  assignment done

--- Assignment-Exercises/JS-6_Objects-and-Methods/script.js
+++ Assignment-Exercises/JS-6_Objects-and-Methods/script.js
@@ -1,8 +1,16 @@
 // Step 1: Create a car object
-
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2021
+};
 
 // Step 2: Create a function to describe the car
-
+function getDescription(){
+    console.log(
+        `This car is a ${car.make} ${car.model} ${car.year}`
+    )
+}
 
 // Step 3: Call the function and display the result
-
+getDescription();
